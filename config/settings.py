4ecda7eb6b0c327b3f1942b941cab18fc7e612cf
Nikulin <s.b.nikuliny@gmail.com>
2023-04-30 19:39:57 +0700

    ch9 email is not sent

--- config/settings.py
+++ config/settings.py
@@ -172,3 +172,4 @@ STATICFILES_FINDERS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_FROM_EMAIL = 'admin@djangobookstore.com'
