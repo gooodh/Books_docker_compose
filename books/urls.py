49f55066cb6b8e794ece4dc8e1e455c293de36c1
Nikulin <s.b.nikuliny@gmail.com>
2023-06-13 16:12:33 +0700

    ch10 add url - UUID

--- books/urls.py
+++ books/urls.py
@@ -6,5 +6,5 @@ from .views import BookListView, BookDetailView # new
 
 urlpatterns = [
 path('', BookListView.as_view(), name='book_list'),
-path('<int:pk>/', BookDetailView.as_view(), name='book_detail'), # new
+path('<uuid:pk>/', BookDetailView.as_view(), name='book_detail'), # new
 ]
